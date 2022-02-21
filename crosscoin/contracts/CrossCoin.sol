ba285702503185c3dd1bf14e2c1a6912a41db889
NorwegianGoat <riccardomioli@gmail.com>
2022-02-21 17:41:12 +0100

    Added mint function to crosscoin

--- crosscoin/contracts/CrossCoin.sol
+++ crosscoin/contracts/CrossCoin.sol
@@ -4,4 +4,8 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CrossCoin is ERC20 {
     constructor() ERC20("CrossCoin", "CC") {}
+
+    function mint(address beneficiary, uint256 quantity) external {
+        _mint(beneficiary, quantity);
+    }
 }
