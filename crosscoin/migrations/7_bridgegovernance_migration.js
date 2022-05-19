dcc78b83406cea9e0af8cbb9456185492f9cabc8
NorwegianGoat <riccardomioli@gmail.com>
2022-05-19 12:57:13 +0200

    updated bridge governance

--- crosscoin/migrations/7_bridgegovernance_migration.js
+++ crosscoin/migrations/7_bridgegovernance_migration.js
@@ -1,5 +1,9 @@
 const BridgeGovernance = artifacts.require("BridgeGovernance");
+//Quorum is fixed at 50%
+const Quorum = 50
+//1 of the chain currency is requested to join
+const Collateral = 1
 
 module.exports = function (deployer) {
-  deployer.deploy(BridgeGovernance);
+  deployer.deploy(BridgeGovernance, Quorum, Collateral);
 };
