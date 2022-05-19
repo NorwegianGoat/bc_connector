26c47fe2fb9923cbf4cfaac625c8b0f5e5765f70
NorwegianGoat <riccardomioli@gmail.com>
2022-05-19 14:36:43 +0200

    Fixed deploy

--- crosscoin/migrations/7_bridgegovernance_migration.js
+++ crosscoin/migrations/7_bridgegovernance_migration.js
@@ -1,9 +1,10 @@
 const BridgeGovernance = artifacts.require("BridgeGovernance");
 //Quorum is fixed at 50%
 const Quorum = 50
-//1 of the chain currency is requested to join
-const Collateral = 1
+//1 of the chain currency is requested to join. Is expressed in wei
+const Collateral = "1000000000000000000"
 
 module.exports = function (deployer) {
-  deployer.deploy(BridgeGovernance, Quorum, Collateral);
+  deployer.deploy(BridgeGovernance, Quorum, Collateral,
+    { value: Collateral });
 };
