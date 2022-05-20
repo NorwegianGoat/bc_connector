ce52292641c86b0619abb3795f0a1c1c0bfc1ff0
NorwegianGoat <riccardomioli@gmail.com>
2022-05-20 08:13:31 +0000

    svaing proposal data

--- scripts/bridge_governance.py
+++ scripts/bridge_governance.py
@@ -2,19 +2,21 @@ from typing import List
 from web3 import HTTPProvider, Web3
 from web3.middleware.geth_poa import geth_poa_middleware
 from utils.contracts_utils import load_abi
-import logging
 from enum import Enum
+import logging
+import os
+import json
+
+SECRET_KEY_PATH = "resources/.secret"
+PROPOSALS_BASE_PATH = "resources/proposals"
+BRIDGE_ADDR = "0xAd28ab39509672F4D621206710654bd875D5fEa2"
+BRIDGE_GOVERNANCE_ADDR = "0x8783E2Fdf8C04dFA64DFC15eba46Bebcc6296C1f"
 
 
 class Vote(Enum):
-    AGAINST = 0,
-    FOR = 1,
+    AGAINST = 0
+    FOR = 1
     ABSTAIN = 2
-
-
-SECRET_KEY_PATH = "resources/.secret"
-BRIDGE_ADDR = "0xAd28ab39509672F4D621206710654bd875D5fEa2"
-BRIDGE_GOVERNANCE_ADDR = "0x47D15Efc24667918225edfB3dd193bf36A002F20"
 
 
 class BridgeGovernance():
@@ -49,22 +51,20 @@ class BridgeGovernance():
 
     def join_governance(self, address: str, amount: int):
         function = self.bridge_governance.functions.join(address)
-        logging.info(self._call_function(function, amount))
+        self._call_function(function, amount)
 
     def vote_proposal(self, proposal_id: int, vote: Vote):
         function = self.bridge_governance.functions.castVote(
             proposal_id, vote.value)
-        logging.info(self._call_function(function))
+        self._call_function(function)
 
     # This command has some effect only if the function is called with the admin account
     def drop_priviledges(self):
         function = self.bridge.functions.renounceAdmin(
             self.bridge_governance_addr)
-        logging.info(self._call_function(function))
+        self._call_function(function)
 
-    def remap_proposal(self, resource_addresses: List[str], description: str):
-        calldata = self.bridge.encodeABI(
-            fn_name="adminSetResource", args=resource_addresses)
+    def _make_proposal(self, calldata, resource_addresses: List[str], description: str):
         logging.info("calldata is:" + str(calldata))
         function = self.bridge_governance.functions.propose(
             [self.bridge.address], [0], [calldata], description)
@@ -72,10 +72,26 @@ class BridgeGovernance():
         if receipt["status"] == 1:
             event = bridge.bridge_governance.events.ProposalCreated.getLogs(
                 fromBlock=receipt["blockNumber"])
-            logging.info("Your proposal id is: " +
-                         str(event[0]["args"]["proposalId"]))
+            proposal_id = str(event[0]["args"]["proposalId"])
+            proposal = {"proposal_id": proposal_id,
+                        "targets": resource_addresses, "values": [0],
+                        "description": description, }
+            with open(os.path.join(PROPOSALS_BASE_PATH, proposal_id + ".json"), "w") as f:
+                json.dump(obj=proposal, fp=f)
+            logging.info("Your proposal id is: " + str(proposal_id))
+            return int(proposal_id)
         else:
-            logging.info("Status is 0, has this proposal been issued before?")
+            logging.info(
+                "Status is 0, you may have not the priviledges for this action or the proposal was issued previously.")
+
+    def execute_proposal(self, proposal_id: int):
+        pass
+
+    def remap_proposal(self, resource_addresses: List[str], description: str):
+        calldata = self.bridge.encodeABI(
+            fn_name="adminSetResource", args=resource_addresses)
+        return self._make_proposal(calldata, resource_addresses, description)
+        
 
 
 if __name__ == "__main__":
@@ -83,6 +99,7 @@ if __name__ == "__main__":
     node = Web3(HTTPProvider("http://192.168.1.110:8545"))
     bridge = BridgeGovernance(
         node, BRIDGE_GOVERNANCE_ADDR, BRIDGE_ADDR)
-    bridge.remap_proposal(
+    proposal_id = bridge.remap_proposal(
         [BRIDGE_ADDR, "0xd8de56dd1db472be57d5840cb8d8d5961c69601e8d8d8a0c97a57c9ae8cb0f0f", BRIDGE_ADDR], "test")
-    #bridge.vote_proposal(1, Vote.FOR)
+    bridge.vote_proposal(
+        proposal_id, Vote.FOR)
