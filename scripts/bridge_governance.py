4d48dae689a4e9a0427b934c80b1fd5874e661e4
NorwegianGoat <riccardomioli@gmail.com>
2022-05-20 13:53:04 +0000

    redeploy bridge for test, remap now works. Example file

--- scripts/bridge_governance.py
+++ scripts/bridge_governance.py
@@ -6,11 +6,12 @@ from enum import Enum
 import logging
 import os
 import json
+import time
 
 SECRET_KEY_PATH = "resources/.secret"
 PROPOSALS_BASE_PATH = "resources/proposals"
-BRIDGE_ADDR = "0xAd28ab39509672F4D621206710654bd875D5fEa2"
-BRIDGE_GOVERNANCE_ADDR = "0x8783E2Fdf8C04dFA64DFC15eba46Bebcc6296C1f"
+BRIDGE_ADDR = "0x63Eef9C40ab428CD48Be40Bc2517b168f67B750f"
+BRIDGE_GOVERNANCE_ADDR = "0x0A03fBe04D9cd154052db3394393Ca690096BAE0"
 
 
 class Vote(Enum):
@@ -38,7 +39,7 @@ class BridgeGovernance():
     def _call_function(self, function, amount: int = 0):
         t_dict = {"chainId": self.node_endpoint.eth.chain_id,
                   "nonce": self.node_endpoint.eth.get_transaction_count(self.account.address, 'pending'),
-                  "gasPrice": self.node_endpoint.toWei(10, "gwei"),
+                  "gasPrice": self.node_endpoint.toWei(1, "gwei"),
                   "gas": 1000000}
         tx = function.buildTransaction(t_dict)
         signed_tx = self.node_endpoint.eth.account.sign_transaction(
@@ -46,36 +47,34 @@ class BridgeGovernance():
         tx_hash = self.node_endpoint.eth.send_raw_transaction(
             signed_tx.rawTransaction)
         receipt = self.node_endpoint.eth.wait_for_transaction_receipt(tx_hash)
-        logging.info("root update tx_receipt: " + str(receipt))
+        logging.info("tx_receipt: " + str(receipt))
         return receipt
 
+    # Specify an account to be added to the governance
     def join_governance(self, address: str, amount: int):
         function = self.bridge_governance.functions.join(address)
         self._call_function(function, amount)
 
+    # Proposal lifecycle functions: vote, make and execute
     def vote_proposal(self, proposal_id: int, vote: Vote):
         function = self.bridge_governance.functions.castVote(
             proposal_id, vote.value)
         self._call_function(function)
 
-    # This command has some effect only if the function is called with the admin account
-    def drop_priviledges(self):
-        function = self.bridge.functions.renounceAdmin(
-            self.bridge_governance_addr)
-        self._call_function(function)
-
-    def _make_proposal(self, calldata, resource_addresses: List[str], description: str):
+    def _make_proposal(self, targets: List[str], amount: List[int], calldata: List[str], description: str):
         logging.info("calldata is:" + str(calldata))
         function = self.bridge_governance.functions.propose(
-            [self.bridge.address], [0], [calldata], description)
+            targets, amount, calldata, description)
         receipt = self._call_function(function)
         if receipt["status"] == 1:
             event = bridge.bridge_governance.events.ProposalCreated.getLogs(
                 fromBlock=receipt["blockNumber"])
             proposal_id = str(event[0]["args"]["proposalId"])
             proposal = {"proposal_id": proposal_id,
-                        "targets": resource_addresses, "values": [0],
-                        "description": description, }
+                        "targets": targets, "values": amount,
+                        "calldata": calldata,
+                        "description": description,
+                        "description_hash": Web3.keccak(text=description).hex()}
             with open(os.path.join(PROPOSALS_BASE_PATH, proposal_id + ".json"), "w") as f:
                 json.dump(obj=proposal, fp=f)
             logging.info("Your proposal id is: " + str(proposal_id))
@@ -85,13 +84,26 @@ class BridgeGovernance():
                 "Status is 0, you may have not the priviledges for this action or the proposal was issued previously.")
 
     def execute_proposal(self, proposal_id: int):
-        pass
+        with open(os.path.join(PROPOSALS_BASE_PATH, str(proposal_id) + ".json")) as f:
+            proposal = json.load(fp=f)
+        print(proposal["targets"], proposal["values"],
+              proposal["calldata"], proposal["description_hash"])
+        function = self.bridge_governance.functions.execute(
+            proposal["targets"], proposal["values"], proposal["calldata"], proposal["description_hash"])
+        self._call_function(function)
+
+    # This command has some effect only if the function is called with the admin account
+    # It drops priviledges on the bridge from the individual and it gives them to the
+    # bridge governance account.
+    def drop_priviledges(self):
+        function = self.bridge.functions.renounceAdmin(
+            self.bridge_governance_addr)
+        self._call_function(function)
 
     def remap_proposal(self, resource_addresses: List[str], description: str):
         calldata = self.bridge.encodeABI(
             fn_name="adminSetResource", args=resource_addresses)
-        return self._make_proposal(calldata, resource_addresses, description)
-        
+        return self._make_proposal([self.bridge.address], [0], [calldata], description)
 
 
 if __name__ == "__main__":
@@ -99,7 +111,12 @@ if __name__ == "__main__":
     node = Web3(HTTPProvider("http://192.168.1.110:8545"))
     bridge = BridgeGovernance(
         node, BRIDGE_GOVERNANCE_ADDR, BRIDGE_ADDR)
+    # Handler, resource id, token
     proposal_id = bridge.remap_proposal(
-        [BRIDGE_ADDR, "0xd8de56dd1db472be57d5840cb8d8d5961c69601e8d8d8a0c97a57c9ae8cb0f0f", BRIDGE_ADDR], "test")
-    bridge.vote_proposal(
-        proposal_id, Vote.FOR)
+        ["0xCC08eac119e25f6E365C25d61eA60bC8e74B681e", "0xd8de56dd1db472be57d5840cb8d8d5961c69601e8d8d8a0c97a57c9ae8cb0f0f",
+            "0x3ab2A28A2a95FA7bbBdF8DfED9e6D945E99dDf38"],
+        "Let's map the bridge! Again!")
+    bridge.vote_proposal(proposal_id, Vote.FOR)
+    bridge.drop_priviledges()
+    time.sleep(20)
+    bridge.execute_proposal(proposal_id)
