c42fda1a6ce2fd1824edc5defbcd2790d2e11ce1
NorwegianGoat <riccardomioli@gmail.com>
2022-06-01 10:24:15 +0000

    Added tests

--- scripts/tests.py
+++ scripts/tests.py
@@ -2,15 +2,16 @@ import unittest
 from utils.truffle_utils import Truffle
 from bridge_governance import BridgeGovernance, Vote
 from web3 import Web3, HTTPProvider
-from utils.resource_manager import add_contract
+from utils.resource_manager import add_contract, available_contracts, save_binding
 from model.contract import ContractTypes
 import time
 import relayer_client
-from threading import Thread
+from multiprocessing import Process
 from relayer_manager import RelayerManager
 from utils.cc_redeem import redeem_tokens
 import os
 from utils.cb_wrapper import CBWrapper
+from utils.sys_mod import ssh_helper
 
 
 NODE0_ENDPOINT = "http://192.168.1.110:8545"
@@ -28,65 +29,58 @@ class TestPatches(unittest.TestCase):
         deployer = Truffle("goatchain0")
         cls.contracts_n0 = deployer.run_migration(9, 9)
         deployer = Truffle("goatchain1")
-        cls.contracts_n1_wrong = deployer.run_migration(9, 9)
-        deployer = Truffle("goatchain1")
-        cls.contracts_n1_good = deployer.run_migration(9, 9)
+        cls.contracts_n1 = deployer.run_migration(9, 9)
         # Configure node and saving deployed data
         cls.n0 = Web3(HTTPProvider(NODE0_ENDPOINT))
-        cls.n1_wrong = Web3(HTTPProvider(NODE1_ENDPOINT))
-        cls.n1_good = Web3(HTTPProvider(NODE1_ENDPOINT))
+        cls.n1 = Web3(HTTPProvider(NODE1_ENDPOINT))
         add_contract(ContractTypes.BRIDGE.value,
                      cls.contracts_n0[1], cls.n0.eth.chain_id, int(time.time()))
         add_contract(ContractTypes.ERC20_HANDLER.value,
                      cls.contracts_n0[2], cls.n0.eth.chain_id, int(time.time()))
         add_contract(ContractTypes.ERC20.value,
                      cls.contracts_n0[0], cls.n0.eth.chain_id, int(time.time()))
+        contracts = available_contracts(100, ContractTypes.ERC20)
+        save_binding(RES_ID, contracts['bridge'].id,
+                     contracts['handler'].id, contracts['target'].id, 100)
         add_contract(ContractTypes.BRIDGE.value,
-                     cls.contracts_n1_good[1], cls.n1_good.eth.chain_id, int(time.time()))
+                     cls.contracts_n1[1], cls.n1.eth.chain_id, int(time.time()))
         add_contract(ContractTypes.ERC20_HANDLER.value,
-                     cls.contracts_n1_good[2], cls.n1_good.eth.chain_id, int(time.time()))
+                     cls.contracts_n1[2], cls.n1.eth.chain_id, int(time.time()))
         add_contract(ContractTypes.ERC20.value,
-                     cls.contracts_n1_good[0], cls.n1_good.eth.chain_id, int(time.time()))
+                     cls.contracts_n1[0], cls.n1.eth.chain_id, int(time.time()))
+        contracts = available_contracts(45, ContractTypes.ERC20)
+        save_binding(RES_ID, contracts['bridge'].id,
+                     contracts['handler'].id, contracts['target'].id, 45)
         # Configuring test accounts
         with open(PKEY_PATH) as f:
             key = f.readline().strip()
         cls.alice = cls.n0.eth.account.from_key(key)
         cls.governance_n0 = BridgeGovernance(cls.alice,
                                              cls.n0, cls.contracts_n0[-1], cls.contracts_n0[1])
-        cls.governance_n1_wrong = BridgeGovernance(cls.alice,
-                                                   cls.n1_wrong, cls.contracts_n1_wrong[-1], cls.contracts_n1_wrong[-1])
-        cls.governance_n1_good = BridgeGovernance(cls.alice,
-                                                  cls.n1_good, cls.contracts_n1_good[-1], cls.contracts_n1_good[1])
+        cls.governance_n1 = BridgeGovernance(cls.alice,
+                                             cls.n1, cls.contracts_n1[-1], cls.contracts_n1[1])
         with open(TRUDY_PKEY_PATH) as f:
             key = f.readline().strip()
         cls.trudy = cls.n0.eth.account.from_key(key)
         # Drop admin privileges and give them to the governance
         cls.governance_n0.drop_priviledges()
-        cls.governance_n1_wrong.drop_priviledges()
-        cls.governance_n1_good.drop_priviledges()
+        cls.governance_n1.drop_priviledges()
         # Configure chain 1 bridges
-        proposal_id = cls.governance_n1_good.remap_proposal([cls.contracts_n1_good[0], RES_ID,
-                                                             cls.contracts_n1_good[2]],
-                                                            "This is a good mapping.")
-        cls.governance_n1_good.vote_proposal(proposal_id, Vote.FOR)
+        proposal_id = cls.governance_n1.remap_proposal([cls.contracts_n1[0], RES_ID,
+                                                        cls.contracts_n1[2]],
+                                                       "This is a good mapping.")
+        cls.governance_n1.vote_proposal(proposal_id, Vote.FOR)
         time.sleep(WAIT)
-        receipt = cls.governance_n1_good.execute_proposal(proposal_id)
-        proposal_id = cls.governance_n1_wrong.remap_proposal([cls.contracts_n1_wrong[0], RES_ID,
-                                                              cls.contracts_n1_wrong[1]],
-                                                             "This is a wrong mapping.")
-        cls.governance_n1_wrong.vote_proposal(proposal_id, Vote.FOR)
-        time.sleep(WAIT)
-        receipt = cls.governance_n1_wrong.execute_proposal(proposal_id)
-        # Generate some deposits on chain 1
-        redeem_tokens(cls.n1_good, cls.alice, 1, ContractTypes.ERC20)
+        receipt = cls.governance_n1.execute_proposal(proposal_id)
+        # Start relayer server and config cb wrapper
         cls.cb_wrapper = CBWrapper()
-        cls.cb_wrapper.deposit("http://192.168.1.120:8545", cls.alice.key, 10,
-                               ContractTypes.ERC20, 1, 100, cls.contracts_n1_good[1], cls.alice.address, RES_ID)
-        # Start relayer server
-        thread = Thread(target=RelayerManager, args=(
-            "http://192.168.1.110", 23456, NODE0_ENDPOINT, cls.contracts_n0[3]))
-        thread.start()
-        #subprocess.run(['nohup', 'python3', 'relayer_manager.py', '&'])
+        cls.relayer_manager = Process(target=RelayerManager, args=(
+            "192.168.1.110", 23456, NODE0_ENDPOINT, cls.contracts_n0[3]))
+        cls.relayer_manager.start()
+
+    @classmethod
+    def tearDown(cls):
+        cls.relayer_manager.terminate()
 
     # A non admin user should not be able to make a proposal
     def test_00proposalNonAdmin(self):
@@ -145,26 +139,71 @@ class TestPatches(unittest.TestCase):
         self.assertEqual(receipt['status'], 0)
 
     # A relayer ask for remap. Good storage.
-    def test_05askRemapGoodStorage(self):
+    def test_05askRemapGoodStorageAndCode(self):
+        # Generate some deposits on chain 1
+        redeem_tokens(TestPatches.n1, TestPatches.alice,
+                      TestPatches.n1.toWei(1, 'ether'), ContractTypes.ERC20)
+        TestPatches.cb_wrapper.deposit("http://192.168.1.120:8545", TestPatches.alice.key.hex(), 1000000,
+                                       ContractTypes.ERC20, TestPatches.n1.toWei(
+                                           1, 'ether'), 100,
+                                       TestPatches.contracts_n1[1], TestPatches.alice.address, RES_ID)
         history = "resources/contract_storage_tries/45.json"
         if os.path.exists(history):
             os.remove(history)
         response = relayer_client.ask_remap(
             "http://192.168.1.110:23456", "http://192.168.1.120:8545",
-            TestPatches.n1_good.eth.get_block("latest")['number']-1000, TestPatches.contracts_n1_good[1], RES_ID)
+            TestPatches.n1.eth.get_block("latest")['number']-1000, TestPatches.contracts_n1[1], RES_ID)
         self.assertEqual(response["response"], "OK")
 
     # A relayer ask for a remap, but his storage is wrong. Should fail.
     def test_06askRemapWrongStorage(self):
-        pass
+        CHAIN1 = ['192.168.1.120', '192.168.1.121',
+                  '192.168.1.122', '192.168.1.123']
+        redeem_tokens(TestPatches.n1, TestPatches.alice,
+                      TestPatches.n1.toWei(1, 'ether'), ContractTypes.ERC20)
+        for node in CHAIN1:
+            ssh_helper(node, 'root', ['cd', 'edge_utils', '&&', 'python3', 'helper.py',
+                                      'halt_node', '&&', 'python3', 'helper.py', 'backup', '--backup_name',
+                                      'malicious_rollback', '--override', 'true', '&&', 'python3', 'helper.py', 'start_validator',
+                                      '--ip', node, '1>/dev/null', '2>/dev/null'])
+        TestPatches.cb_wrapper.deposit("http://192.168.1.120:8545", TestPatches.alice.key.hex(), 1000000,
+                                       ContractTypes.ERC20, TestPatches.n1.toWei(
+                                           1, 'ether'), 100,
+                                       TestPatches.contracts_n1[1], TestPatches.alice.address, RES_ID)
+        response = relayer_client.ask_remap(
+            "http://192.168.1.110:23456", "http://192.168.1.120:8545",
+            TestPatches.n1.eth.get_block("latest")['number']-1000, TestPatches.contracts_n1[1], RES_ID)
+        # TODO: RM RELAYER?
+        # Restore the old state
+        for node in CHAIN1:
+            ssh_helper(node, 'root', ['cd', 'edge_utils', '&&', 'python3', 'helper.py', 'halt_node', '&&',
+                                      'python3', 'helper.py', 'restore', '--backup_path', './edge/malicious_rollback',
+                                      '&&', 'python3', 'helper.py', 'start_validator', '--ip', node, '1>/dev/null', '2>/dev/null'])
+        TestPatches.cb_wrapper.deposit("http://192.168.1.120:8545", TestPatches.alice.key.hex(), 1000000,
+                                       ContractTypes.ERC20, TestPatches.n1.toWei(
+                                           1, 'ether'), 100,
+                                       TestPatches.contracts_n1[1], TestPatches.alice.address, RES_ID)
+        response = relayer_client.ask_remap(
+            "http://192.168.1.110:23456", "http://192.168.1.120:8545",
+            TestPatches.n1.eth.get_block("latest")['number']-1000, TestPatches.contracts_n1[1], RES_ID)
+        self.assertEqual(response["response"], "NOK")
 
-    # A relayer ask for remap. Smart contract code is not equal. Should fail.
+    # A relayer asks for remap. Smart contract code is not equal. Should fail.
     def test_07askRemapWrongCode(self):
-        pass
-
-    # A relayer ask for remap, smart contract code matches. Should pass.
-    def test_08askRemapGoodCode(self):
-        pass
+        # We intentionally break the origin bridge
+        proposal_id = TestPatches.governance_n1.remap_proposal([TestPatches.contracts_n1[0], RES_ID,
+                                                        TestPatches.contracts_n1[0]],
+                                                       "This is a good mapping.")
+        TestPatches.governance_n1.vote_proposal(proposal_id, Vote.FOR)
+        time.sleep(WAIT)
+        receipt = TestPatches.governance_n1.execute_proposal(proposal_id)
+        history = "resources/contract_storage_tries/45.json"
+        if os.path.exists(history):
+            os.remove(history)
+        response = relayer_client.ask_remap(
+            "http://192.168.1.110:23456", "http://192.168.1.120:8545",
+            TestPatches.n1.eth.get_block("latest")['number']-1000, TestPatches.contracts_n1[1], RES_ID)
+        self.assertEqual(response["response"], "NOK")
 
     # Admin tries to refund an user sending wrong data to the smart contract. Should fail.
     def test_09refundWrongData(self):
